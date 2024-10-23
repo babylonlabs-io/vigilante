ae53d68466eb262c779f567d4d4c34d29c65ba44
Lazar <lukic.lazar@yahoo.com>
2024-10-23 16:53:44 +0200

    use proper tag for bbnb

--- e2etest/container/config.go
+++ e2etest/container/config.go
@@ -24,13 +24,13 @@ const (
 
 // NewImageConfig returns ImageConfig needed for running e2e test.
 func NewImageConfig(t *testing.T) ImageConfig {
-	_, err := testutil.GetBabylonVersion()
+	babylonVersion, err := testutil.GetBabylonVersion()
 	require.NoError(t, err)
 
 	return ImageConfig{
 		BitcoindRepository: dockerBitcoindRepository,
 		BitcoindVersion:    dockerBitcoindVersionTag,
 		BabylonRepository:  dockerBabylondRepository,
-		BabylonVersion:     "6a0ccacc41435a249316a77fe6e1e06aeb654d13", // todo(lazar): remove this when we have a tag
+		BabylonVersion:     babylonVersion,
 	}
 }
