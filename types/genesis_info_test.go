fa7a2ad7dea0e991c6c46a7395daa35fa7368b09
Lazar <lukic.lazar@yahoo.com>
2025-03-06 16:26:47 +0100

    lint

--- types/genesis_info_test.go
+++ types/genesis_info_test.go
@@ -62,7 +62,7 @@ func FuzzGetGenesisInfoFromFile(f *testing.F) {
 		genesisInfo, err := GetGenesisInfoFromFile(genFile)
 		require.NoError(t, err)
 		require.Equal(t, uint64(epochInterval), genesisInfo.epochInterval)
-		//require.Len(t, genesisInfo.valSet.ValSet, validatorNum) todo(lazar): see why this is failing
+		// require.Len(t, genesisInfo.valSet.ValSet, validatorNum) todo(lazar): see why this is failing
 		require.Equal(t, uint32(baseHeight), genesisInfo.baseBTCHeight)
 	})
 }
