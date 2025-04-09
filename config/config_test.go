eb0d75f898f3419e71b2e62d9c33407442631565
Lazar <12626340+Lazar955@users.noreply.github.com>
2025-04-09 11:02:55 +0200

    fix: default config saving (#315)
    We need this in the `dump-cfg` cmd

--- config/config_test.go
+++ config/config_test.go
@@ -1,3 +1,25 @@
 package config_test
 
-// TODO: tests on config
+import (
+	"fmt"
+	"github.com/babylonlabs-io/vigilante/config"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// TestDefaultConfig tests the default configuration of the Vigilante application.
+// SaveToYAML is used in the dump-cfg command, we want to make sure that all properties names are correctly saved
+func TestDefaultConfig(t *testing.T) {
+	t.Parallel()
+
+	defaultCfg := config.DefaultConfig()
+	cfgPath := fmt.Sprintf("%s/%s", t.TempDir(), "config.yaml")
+
+	err := defaultCfg.SaveToYAML(cfgPath)
+	require.NoError(t, err, "failed to save default config to yaml")
+
+	loadedCfg, err := config.New(cfgPath)
+	require.NoError(t, err, "failed to load config from yaml")
+
+	require.Equal(t, *defaultCfg, loadedCfg, "loaded config does not match default config")
+}
