c7de77453893bb76428023c7a95edc7508960957
Lazar <lukic.lazar@yahoo.com>
2025-08-29 15:42:23 +0200

    lint fix

--- btcstaking-tracker/btcslasher/store/store.go
+++ btcstaking-tracker/btcslasher/store/store.go
@@ -107,6 +107,7 @@ func (s *SlasherStore) PutHeight(height uint64) error {
 func uint64ToBytes(n uint64) []byte {
 	buf := make([]byte, 8)
 	binary.LittleEndian.PutUint64(buf, n)
+
 	return buf
 }
 
