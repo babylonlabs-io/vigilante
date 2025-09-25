1711b51ce050a7dd90324e4f4e0c96c512860fe5
samricotta <37125168+samricotta@users.noreply.github.com>
2025-09-25 16:45:33 +0300

    reviewal comments

--- monitor/activation_unbonding_monitor.go
+++ monitor/activation_unbonding_monitor.go
@@ -1,9 +1,12 @@
 package monitor
 
 import (
+	"github.com/avast/retry-go/v4"
 	btcstakingtypes "github.com/babylonlabs-io/babylon/v3/x/btcstaking/types"
 	"github.com/babylonlabs-io/vigilante/btcclient"
 	"github.com/babylonlabs-io/vigilante/config"
+	"github.com/lightningnetwork/lnd/chainntnfs"
+	"time"
 )
 
 type ActivationUnbondingMonitor struct {
@@ -20,36 +23,12 @@ func NewActivationUnbondingMonitor(babylonClient BabylonBTCStakingClient, btcCli
 	}
 }
 
-func (m *ActivationUnbondingMonitor) GetVerifiedDelegations() ([]Delegation, error) {
+func (m *ActivationUnbondingMonitor) GetDelegationsByStatus(status btcstakingtypes.BTCDelegationStatus) ([]Delegation, error) {
 	cursor := []byte(nil)
 	var allDelegations []Delegation
 	for {
 		del, nextCursor, err := m.babylonClient.DelegationsByStatus(
-			btcstakingtypes.BTCDelegationStatus_VERIFIED,
-			cursor, 100,
-		)
-		if err != nil {
-			return nil, err
-		}
-
-		allDelegations = append(allDelegations, del...)
-
-		if nextCursor == nil {
-			break
-		}
-		cursor = nextCursor
-	}
-
-	return allDelegations, nil
-}
-
-func (m *ActivationUnbondingMonitor) GetActiveDelegations() ([]Delegation, error) {
-	cursor := []byte(nil)
-	var allDelegations []Delegation
-	for {
-		del, nextCursor, err := m.babylonClient.DelegationsByStatus(
-			btcstakingtypes.BTCDelegationStatus_ACTIVE,
-			cursor, 100,
+			status, cursor, 100,
 		)
 		if err != nil {
 			return nil, err
@@ -74,7 +53,17 @@ func (m *ActivationUnbondingMonitor) CheckKDeepConfirmation(delegation *Delegati
 	outputScript := delegation.StakingTx.TxOut[delegation.StakingOutputIdx].PkScript
 	stakingTxHash := delegation.StakingTx.TxHash()
 
-	details, status, err := m.btcClient.TxDetails(&stakingTxHash, outputScript)
+	var details *chainntnfs.TxConfirmation
+	var status btcclient.TxStatus
+
+	err := retry.Do(func() error {
+		var err error
+		details, status, err = m.btcClient.TxDetails(&stakingTxHash, outputScript)
+		return err
+	},
+		retry.Attempts(3),
+		retry.Delay(1*time.Second))
+
 	if err != nil {
 		return false, err
 	}
