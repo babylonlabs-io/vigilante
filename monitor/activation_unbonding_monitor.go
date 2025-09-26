fcfb27e5fb1e7e769b2fd0cf1b34224471a11036
samricotta <37125168+samricotta@users.noreply.github.com>
2025-09-26 16:40:25 +0300

    chore: add Bitcoin client integration and delegation queries (#460)
    - Added Bitcoin client for K-deep confirmations
    - Created adapter for Babylon delegation queries with pagination
    - testing

--- monitor/activation_unbonding_monitor.go
+++ monitor/activation_unbonding_monitor.go
@@ -1,31 +1,90 @@
 package monitor
 
 import (
+	"github.com/avast/retry-go/v4"
 	btcstakingtypes "github.com/babylonlabs-io/babylon/v3/x/btcstaking/types"
-	"github.com/cosmos/cosmos-sdk/types/query"
+	"github.com/babylonlabs-io/vigilante/btcclient"
+	"github.com/babylonlabs-io/vigilante/config"
+	"github.com/lightningnetwork/lnd/chainntnfs"
+	"time"
 )
 
 type ActivationUnbondingMonitor struct {
-	babylonClient BabylonBTCStakingClient
+	babylonClient BabylonAdaptorClient
+	btcClient     btcclient.BTCClient
+	cfg           *config.BTCStakingTrackerConfig
 }
 
-func NewActivationUnbondingMonitor(client BabylonBTCStakingClient) *ActivationUnbondingMonitor {
+func NewActivationUnbondingMonitor(babylonClient BabylonAdaptorClient, btcClient btcclient.BTCClient, cfg *config.BTCStakingTrackerConfig) *ActivationUnbondingMonitor {
 	return &ActivationUnbondingMonitor{
-		babylonClient: client,
+		babylonClient: babylonClient,
+		btcClient:     btcClient,
+		cfg:           cfg,
 	}
 }
 
-func (m *ActivationUnbondingMonitor) GetVerifiedDelegations() (*btcstakingtypes.QueryBTCDelegationsResponse, error) {
-	return m.babylonClient.BTCDelegations(
-		btcstakingtypes.BTCDelegationStatus_VERIFIED, &query.PageRequest{Limit: 100},
-	)
+func (m *ActivationUnbondingMonitor) GetDelegationsByStatus(status btcstakingtypes.BTCDelegationStatus) ([]Delegation, error) {
+	cursor := []byte(nil)
+	var allDelegations []Delegation
+	for {
+		del, nextCursor, err := m.babylonClient.DelegationsByStatus(
+			status, cursor, 100,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		allDelegations = append(allDelegations, del...)
+
+		if nextCursor == nil {
+			break
+		}
+		cursor = nextCursor
+	}
+
+	return allDelegations, nil
 }
 
-func (m *ActivationUnbondingMonitor) GetActiveDelegations() (*btcstakingtypes.QueryBTCDelegationsResponse, error) {
-	return m.babylonClient.BTCDelegations(
-		btcstakingtypes.BTCDelegationStatus_ACTIVE, &query.PageRequest{Limit: 100})
-}
-
-func (m *ActivationUnbondingMonitor) GetDelegationByHash(hash string) (*btcstakingtypes.QueryBTCDelegationResponse, error) {
+func (m *ActivationUnbondingMonitor) GetDelegationByHash(hash string) (*Delegation, error) {
 	return m.babylonClient.BTCDelegation(hash)
+}
+
+func (m *ActivationUnbondingMonitor) CheckKDeepConfirmation(delegation *Delegation) (bool, error) {
+	outputScript := delegation.StakingTx.TxOut[delegation.StakingOutputIdx].PkScript
+	stakingTxHash := delegation.StakingTx.TxHash()
+
+	var details *chainntnfs.TxConfirmation
+	var status btcclient.TxStatus
+
+	err := retry.Do(func() error {
+		var err error
+		details, status, err = m.btcClient.TxDetails(&stakingTxHash, outputScript)
+
+		return err
+	},
+		retry.Attempts(3),
+		retry.Delay(1*time.Second))
+
+	if err != nil {
+		return false, err
+	}
+
+	// if false not confirmed yet
+	if status != btcclient.TxInChain {
+		return false, nil
+	}
+
+	currentHeight, err := m.btcClient.GetBestBlock()
+	if err != nil {
+		return false, err
+	}
+
+	bbnDepth, err := m.babylonClient.GetConfirmationDepth()
+	if err != nil {
+		return false, err
+	}
+
+	confirmations := currentHeight - details.BlockHeight
+
+	return confirmations >= bbnDepth, nil
 }
