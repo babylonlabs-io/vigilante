315edc974f3f70ce1d72b157a66553b36499fb7f
samricotta <37125168+samricotta@users.noreply.github.com>
2025-10-01 14:56:24 +0300

    update for timeout

--- metrics/activation_unbonding.go
+++ metrics/activation_unbonding.go
@@ -8,7 +8,7 @@ import (
 type ActivationUnbondingMonitorMetrics struct {
 	Registry                  *prometheus.Registry
 	ActivationDelayHistogram  prometheus.Histogram
-	ActivationTimeoutsCounter prometheus.Counter
+	ActivationTimeoutsCounter *prometheus.CounterVec
 	TrackedActivationGauge    prometheus.Gauge
 }
 
@@ -23,10 +23,10 @@ func NewActivationUnbondingMonitorMetrics() *ActivationUnbondingMonitorMetrics {
 			Help:    "Time delay between k-deep confirmation and activation",
 			Buckets: []float64{30, 60, 120, 300, 600, 1800, 3600},
 		}),
-		ActivationTimeoutsCounter: registerer.NewCounter(prometheus.CounterOpts{
+		ActivationTimeoutsCounter: registerer.NewCounterVec(prometheus.CounterOpts{
 			Name: "vigilante_activation_timeouts_total",
 			Help: "Number of activation timeouts detected",
-		}),
+		}, []string{"delegation_id"}),
 		TrackedActivationGauge: registerer.NewGauge(prometheus.GaugeOpts{
 			Name: "vigilante_activation_tracked_delegations",
 			Help: "Number of delegations being tracked for activation timing",
